9eb5f04ef6bcee25fd787ff0d19d0f01e04ed05d
Veselin <vesko.kalinov06@gmail.com>
2026-01-28 12:13:43 +0000

    Jan 28, 2026, 1:13 PM

--- Exercism/c/grains/grains.c
+++ Exercism/c/grains/grains.c
@@ -3,5 +3,11 @@
 main()
 {
     unsigned int n;
+
     printf("Square Num: ");
+    scanf("%u", &n);
+
+    printf("\nResults %u squares:\n", n);
+    printf("\nThe number of grains on a given square: %u\n", square(n));
+    printf("\nThe total number of grains on the chessboard: %u\n", total(n));
 }
